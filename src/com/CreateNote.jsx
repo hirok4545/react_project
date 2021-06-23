cd0c1eaf5d3afd75b8ce1d85c14dda8db926bd1d
hirok4545 <hdeka731@gmail.com>
2021-06-23 20:30:42 +0530

    1 file modified

--- src/com/CreateNote.jsx
+++ src/com/CreateNote.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React from 'react';
 
-
-function CreateNote(){
-  return <div> <h1> Create Note page</h1></div>
+function CreateNote() {
+  return (
+    <div className="container">
+      <h1> Create Note page</h1>
+    </div>
+  );
 }
 export default CreateNote;
