74b6ab9bc2cd7746e748efd8bf76926cbbd7d278
hirok4545 <hdeka731@gmail.com>
2021-06-23 20:37:26 +0530

    2 files modified

--- src/com/Home.jsx
+++ src/com/Home.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 
 function Home(){
-  return <div> <h1> Create Home page</h1></div>
+  return <div className="container"> <h1> Create Home page</h1></div>
 }
 export default Home;
--- src/com/Notes.jsx
+++ src/com/Notes.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 
 function Notes(){
-  return <div> <h1> Create Notes page</h1></div>
+  return <div className="container"> <h1> Create Notes page</h1></div>
 }
 export default Notes;
