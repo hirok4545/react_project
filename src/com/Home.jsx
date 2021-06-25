528d26abf0d87da9be8cc8e69d618b7954aaa748
hirok4545 <hdeka731@gmail.com>
2021-06-25 21:28:50 +0530

    1 file modified

--- src/com/Home.jsx
+++ src/com/Home.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 
 
+
+
+
+
+
 function Home(){
-  return <div className="container"> <h1> Create Home page</h1></div>
+  const data={
+    
+        "id": 1, "name": "Apples",  "price": "$2" ,
+         "id": 2, "name": "Peaches", "price": "$5"
+      
+    };
+
+
+  const[status,setStatus]=React.useState(true)
+
+
+
+  
+
+  return <div className="container">
+    {
+      status? <h1> {data.id} </h1>
+      
+      
+      
+      :<h1>No Data toogle is off</h1>
+    }
+    
+    
+    <button onClick={()=>setStatus(!status)}>
+    {
+      status? <h1> button on</h1>:<h1>button off</h1> 
+    }
+    
+    </button>
+    </div>
+
 }
 export default Home;
+
